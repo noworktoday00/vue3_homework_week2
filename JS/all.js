2cfc5fce90fd5bf04944b27b5d9cd3a105ee0d33
VCShen <37541252+noworktoday00@users.noreply.github.com>
2022-01-23 10:30:55 +0800

    Initial commit

---
+++ JS/all.js
@@ -0,0 +1,60 @@
+// const emailInput = document.querySelector("#username");
+// const pwInput = document.querySelector("#password");
+// const loginBtn = document.querySelector("#login");
+
+// const url = "https://vue3-course-api.hexschool.io/v2";
+// const path = "vanmoritz";
+
+// loginBtn.addEventListener("click", login);
+
+// function login() {
+//   const username = emailInput.value;
+//   const password = pwInput.value;
+
+//   const user = {
+//     username,
+//     password,
+//   };
+
+//   axios
+//     .post(`${url}/admin/signin`, user)
+//     .then((res) => {
+//       console.log(res);
+//       const { token, expired } = res.data;
+//       console.log(token, expired);
+//       document.cookie = `vanMoritzToken=${token};expires=${new Date(expired)};`;
+//     })
+//     .catch((err) => {
+//       console.dir(err);
+//     });
+// }
+
+const App = {
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      const loginApi = "https://vue3-course-api.hexschool.io/v2/admin/signin";
+      axios
+        .post(loginApi, this.user)
+        .then((res) => {
+          const { token, expired } = res.data;
+          document.cookie = `vanMoritzToken=${token};expires=${new Date(
+            expired
+          )};`;
+          window.location = "products.html";
+        })
+        .catch((err) => {
+          alert(err.data);
+          // console.dir(err);
+        });
+    },
+  },
+};
+Vue.createApp(App).mount("#app");
